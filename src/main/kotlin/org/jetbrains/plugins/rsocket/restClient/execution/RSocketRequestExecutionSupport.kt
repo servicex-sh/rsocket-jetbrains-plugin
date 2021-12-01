7e29c4a705f4bb710bda210b55da42718f09a0b9
linux_china <libing.chen@gmail.com>
2021-11-30 18:23:53 -0800

    RSocketRequestExecutionSupport

---
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/restClient/execution/RSocketRequestExecutionSupport.kt
@@ -0,0 +1,29 @@
+package org.jetbrains.plugins.rsocket.restClient.execution
+
+import com.intellij.httpClient.execution.common.RequestContext
+import com.intellij.httpClient.execution.common.RequestConverter
+import com.intellij.httpClient.execution.common.RequestExecutionSupport
+import com.intellij.httpClient.execution.common.RequestHandler
+
+@Suppress("UnstableApiUsage")
+class RSocketRequestExecutionSupport : RequestExecutionSupport<RSocketRequest> {
+    companion object {
+        val RSOCKET_REQUEST_TYPES = listOf("RSOCKET", "RPC", "FNF", "STREAM")
+    }
+
+    override fun canProcess(requestContext: RequestContext): Boolean {
+        return RSOCKET_REQUEST_TYPES.contains(requestContext.method)
+    }
+
+    override fun getRequestConverter(): RequestConverter<RSocketRequest> {
+        return RSocketRequestConverter()
+    }
+
+    override fun getRequestHandler(): RequestHandler<RSocketRequest> {
+        return RSocketRequestHandler()
+    }
+
+    override fun supportedMethods(): Collection<String> {
+        return RSOCKET_REQUEST_TYPES
+    }
+}
