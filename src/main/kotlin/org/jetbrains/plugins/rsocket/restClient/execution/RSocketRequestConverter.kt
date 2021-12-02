c66d6cc12fce49ef82c80e1dad326b5ecb2643c4
linux_china <libing.chen@gmail.com>
2021-12-02 11:12:09 -0800

    Polishing

--- src/main/kotlin/org/jetbrains/plugins/rsocket/restClient/execution/RSocketRequestConverter.kt
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/restClient/execution/RSocketRequestConverter.kt
@@ -14,7 +14,11 @@ class RSocketRequestConverter : RequestConverter<RSocketRequest>() {
     override fun psiToCommonRequest(requestPsiPointer: SmartPsiElementPointer<HttpRequest>, substitutor: HttpRequestVariableSubstitutor): RSocketRequest {
         val httpRequest = requestPsiPointer.element
         val headers = httpRequest?.headerFieldList?.associate { it.name.lowercase() to it.getValue(substitutor) }
-        return RSocketRequest(httpRequest?.getHttpUrl(substitutor), httpRequest?.httpMethod, httpRequest?.requestBody?.text, headers)
+        var requestType = httpRequest?.httpMethod
+        if (requestType == "RSOCKET") {
+            requestType = "RPC"
+        }
+        return RSocketRequest(httpRequest?.getHttpUrl(substitutor), requestType, httpRequest?.requestBody?.text, headers)
     }
 
     override fun toExternalFormInner(request: RSocketRequest, fileName: String?): String {
