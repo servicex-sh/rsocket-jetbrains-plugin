529a2510fc6d8e7e33b03a3870faa29f5bf890e1
linux_china <libing.chen@gmail.com>
2021-12-02 09:40:08 -0800

    Introduce rsocketRequestManager

--- src/main/kotlin/org/jetbrains/plugins/rsocket/restClient/execution/RSocketRequestHandler.kt
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/restClient/execution/RSocketRequestHandler.kt
@@ -1,46 +1,28 @@
 package org.jetbrains.plugins.rsocket.restClient.execution
 
 import com.intellij.httpClient.execution.common.CommonClientResponse
-import com.intellij.httpClient.execution.common.CommonClientResponseBody
-import com.intellij.httpClient.execution.common.CommonClientResponseBody.TextStream.Message.Chunk
 import com.intellij.httpClient.execution.common.RequestHandler
 import com.intellij.httpClient.execution.common.RunContext
-import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import reactor.core.publisher.Flux
-import java.time.Duration
+import org.jetbrains.plugins.rsocket.requests.RSocketRequestManager
 
 
 @Suppress("UnstableApiUsage")
 class RSocketRequestHandler : RequestHandler<RSocketRequest> {
 
     override fun execute(request: RSocketRequest, runContext: RunContext): CommonClientResponse {
+        val rsocketRequestManager = runContext.project.getService(RSocketRequestManager::class.java)
         return when (request.httpMethod) {
             "RPC", "RSOCKET" -> {
-                // language=json
-                val jsonText = """
-                    {"id": 1, "nick": "linux_china" }
-                """.trimIndent()
-                RSocketClientResponse(10, CommonClientResponseBody.Text(jsonText, RSocketBodyFileHint.JSON))
+                rsocketRequestManager.requestResponse(request)
             }
             "FNF" -> {
-                RSocketClientResponse(0)
+                rsocketRequestManager.fireAndForget(request)
+            }
+            "M_PUSH" -> {
+                rsocketRequestManager.metadataPush(request)
             }
             "STREAM" -> {
-                val shared = MutableSharedFlow<CommonClientResponseBody.TextStream.Message>(
-                    replay = 1,
-                    onBufferOverflow = BufferOverflow.DROP_OLDEST
-                )
-                val textStream = CommonClientResponseBody.TextStream(shared, RSocketBodyFileHint.TEXT)
-                Flux.just(Chunk("first\n"), Chunk("second\n"), Chunk("third\n"))
-                    .delayElements(Duration.ofSeconds(1))
-                    .doFinally {
-                        shared.tryEmit(CommonClientResponseBody.TextStream.Message.ConnectionClosed.End);
-                    }
-                    .subscribe {
-                        shared.tryEmit(it)
-                    }
-                RSocketClientResponse(10, textStream)
+                rsocketRequestManager.requestStream(request)
             }
             else -> {
                 RSocketClientResponse(0)
