eb2da6ea6ea75af816f0e523592eff4ea6f836b1
linux_china <libing.chen@gmail.com>
2021-12-01 22:47:51 -0800

    Add STREAM support

--- src/main/kotlin/org/jetbrains/plugins/rsocket/restClient/execution/RSocketRequestHandler.kt
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/restClient/execution/RSocketRequestHandler.kt
@@ -2,8 +2,19 @@ package org.jetbrains.plugins.rsocket.restClient.execution
 
 import com.intellij.httpClient.execution.common.CommonClientResponse
 import com.intellij.httpClient.execution.common.CommonClientResponseBody
+import com.intellij.httpClient.execution.common.CommonClientResponseBody.TextStream.Message.Chunk
 import com.intellij.httpClient.execution.common.RequestHandler
 import com.intellij.httpClient.execution.common.RunContext
+import com.intellij.util.concurrency.AppExecutorUtil
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.plus
+import kotlinx.coroutines.reactive.asFlow
+import reactor.core.publisher.Flux
+
 
 @Suppress("UnstableApiUsage")
 class RSocketRequestHandler : RequestHandler<RSocketRequest> {
@@ -15,16 +26,21 @@ class RSocketRequestHandler : RequestHandler<RSocketRequest> {
                 val jsonText = """
                     {"id": 1, "nick": "linux_china" }
                 """.trimIndent()
-                RSocketClientResponse(111, CommonClientResponseBody.Text(jsonText))
+                RSocketClientResponse(10, CommonClientResponseBody.Text(jsonText, RSocketBodyFileHint.JSON))
             }
             "FNF" -> {
-                RSocketClientResponse(111)
+                RSocketClientResponse(0)
             }
             "STREAM" -> {
-                RSocketClientResponse(111, CommonClientResponseBody.Text("Hello, stream!"))
+                val flow = Flux.just(Chunk("first"), Chunk("second"), Chunk("third")).asFlow();
+                val dispatcher = AppExecutorUtil.getAppExecutorService().asCoroutineDispatcher()
+                val scope = CoroutineScope(dispatcher).plus(CoroutineName("RSocket stream call"))
+                val messages = flow.shareIn(scope, SharingStarted.Lazily, 100)
+                val textStream = CommonClientResponseBody.TextStream(messages, RSocketBodyFileHint.TEXT);
+                RSocketClientResponse(10, textStream)
             }
             else -> {
-                RSocketClientResponse(111)
+                RSocketClientResponse(0)
             }
         }
     }
