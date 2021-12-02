df72af5f45838536f9831813b75d3e05c358b6e5
linux_china <libing.chen@gmail.com>
2021-12-02 09:39:36 -0800

    Add M_PUSH for metadata push

--- src/main/kotlin/org/jetbrains/plugins/rsocket/restClient/execution/RSocketRequestExecutionSupport.kt
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/restClient/execution/RSocketRequestExecutionSupport.kt
@@ -8,7 +8,7 @@ import com.intellij.httpClient.execution.common.RequestHandler
 @Suppress("UnstableApiUsage")
 class RSocketRequestExecutionSupport : RequestExecutionSupport<RSocketRequest> {
     companion object {
-        val RSOCKET_REQUEST_TYPES = listOf("RSOCKET", "RPC", "FNF", "STREAM")
+        val RSOCKET_REQUEST_TYPES = listOf("RSOCKET", "RPC", "FNF", "STREAM", "M_PUSH")
     }
 
     override fun canProcess(requestContext: RequestContext): Boolean {
