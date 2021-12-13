bb35a1f105c2ec5b730de46620f9a7293f3458b8
linux_china <libing.chen@gmail.com>
2021-12-13 10:59:14 -0800

    RSocketHeadersInspectionService

---
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/codeInsight/RSocketHeadersInspectionService.kt
@@ -0,0 +1,23 @@
+package org.jetbrains.plugins.rsocket.codeInsight
+
+import com.intellij.httpClient.http.request.codeInsight.HttpRequestIncorrectHttpHeaderInspection
+import com.intellij.openapi.project.Project
+import com.intellij.profile.codeInspection.ProjectInspectionProfileManager
+
+class RSocketHeadersInspectionService(project: Project) {
+    init {
+        val inspectionProfileManager = ProjectInspectionProfileManager.getInstance(project)
+        val inspectionToolWrapper = inspectionProfileManager.currentProfile.getInspectionTool("IncorrectHttpHeaderInspection", project)
+        val httpHeaderInspection = inspectionToolWrapper?.tool as HttpRequestIncorrectHttpHeaderInspection
+        val customHeaders = httpHeaderInspection.getCustomHeaders()
+        if (!(customHeaders.contains("X-AliBroker") || customHeaders.contains("X-ServiceName"))) {
+            customHeaders.add("X-AliBroker")
+            customHeaders.add("X-ServiceName")
+            customHeaders.add("X-InstanceName")
+            customHeaders.add("X-MajorVersion")
+            customHeaders.add("X-ClusterName")
+            customHeaders.add("X-Endpoint-UUID")
+            customHeaders.add("X-Endpoint-Ip")
+        }
+    }
+}
