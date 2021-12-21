369e579ab79a4cd106c542c5f13b14c55b02dfb9
linux_china <libing.chen@gmail.com>
2021-12-20 18:14:39 -0800

    Update to reactor-bom 2020.0.14

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ intellij {
 }
 
 dependencies {
-    implementation(platform("io.projectreactor:reactor-bom:2020.0.13"))
+    implementation(platform("io.projectreactor:reactor-bom:2020.0.14"))
     implementation("io.rsocket:rsocket-core:1.1.1")
     implementation("io.rsocket:rsocket-transport-netty:1.1.1")
     implementation("io.rsocket.broker:rsocket-broker-frames:0.3.0")
