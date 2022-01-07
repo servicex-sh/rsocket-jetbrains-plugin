630c6fb5767377710e263cf13c1b2c3ff6144c8f
linux_china <libing.chen@gmail.com>
2022-01-07 12:17:04 -0800

    Add body as binary

--- src/main/kotlin/org/jetbrains/plugins/rsocket/restClient/execution/RSocketRequest.kt
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/restClient/execution/RSocketRequest.kt
@@ -2,7 +2,9 @@ package org.jetbrains.plugins.rsocket.restClient.execution
 
 import com.intellij.httpClient.execution.common.CommonClientRequest
 import com.intellij.util.queryParameters
+import io.rsocket.metadata.WellKnownMimeType
 import java.net.URI
+import java.util.*
 
 
 @Suppress("UnstableApiUsage")
@@ -12,6 +14,7 @@ class RSocketRequest(override val URL: String?, override val httpMethod: String?
     val metadataMimeTyp: String
     val setupData: String?
     val setupMetadata: String?
+    val metadata: String?
     val acceptMimeType: String?
     val authorization: String?
     val userAgent: String?
@@ -39,13 +42,14 @@ class RSocketRequest(override val URL: String?, override val httpMethod: String?
             }
         }
         rsocketURI = URI.create(tempUri)
-        dataMimeTyp = headers?.get("Content-Type") ?: "application/json"
-        metadataMimeTyp = headers?.get("Metadata-Type") ?: "message/x.rsocket.composite-metadata.v0"
+        dataMimeTyp = headers?.get("Content-Type") ?: WellKnownMimeType.APPLICATION_JSON.string
+        metadataMimeTyp = headers?.get("Metadata-Type") ?: WellKnownMimeType.MESSAGE_RSOCKET_COMPOSITE_METADATA.string
         authorization = headers?.get("Authorization")
         userAgent = headers?.get("User-Agent")
         acceptMimeType = headers?.get("Accept")
-        setupMetadata = headers?.get("Setup-Data")
-        setupData = headers?.get("Setup-Metadata")
+        setupMetadata = headers?.get("Setup-Metadata")
+        setupData = headers?.get("Setup-Data")
+        metadata = headers?.get("Metadata")
     }
 
     fun routingMetadata(): List<String> {
@@ -78,8 +82,39 @@ class RSocketRequest(override val URL: String?, override val httpMethod: String?
 
     fun getWebsocketRequestURI(): URI {
         var connectionURL = rsocketURI.toString()
-        connectionURL = connectionURL.substring(0, connectionURL.indexOf("/rsocket") + 8)
+        if (connectionURL.contains("/rsocket/")) { //rsocket as ws path
+            connectionURL = connectionURL.substring(0, connectionURL.indexOf("/rsocket") + 8)
+        } else { // without /rsocket
+            connectionURL = connectionURL.substring(0, connectionURL.indexOf('/', 10) + 1)
+        }
         connectionURL = connectionURL.replace("http://", "ws://").replace("https://", "wss://")
         return URI.create(connectionURL)
     }
+
+    fun body(): ByteArray {
+        return if (textToSend == null) {
+            ByteArray(0)
+        } else if (textToSend.startsWith("data:")) {
+            val base64Text = textToSend.substring(textToSend.indexOf(",") + 1).trim()
+            Base64.getDecoder().decode(base64Text);
+        } else {
+            if (dataMimeTyp.startsWith("application/json") && textToSend.startsWith("\"")) {
+                textToSend.trim('"').toByteArray(Charsets.UTF_8)
+            } else {
+                textToSend.toByteArray(Charsets.UTF_8)
+            }
+        }
+    }
+
+    fun textToBytes(text: String?): ByteArray {
+        return if (text == null) {
+            ByteArray(0)
+        } else if (text.startsWith("data:")) {
+            val base64Text = text.substring(text.indexOf(",") + 1).trim()
+            Base64.getDecoder().decode(base64Text);
+        } else {
+            text.toByteArray(Charsets.UTF_8)
+        }
+    }
+
 }
