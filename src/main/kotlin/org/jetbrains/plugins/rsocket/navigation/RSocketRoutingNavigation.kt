78114ec3dd406b58b455d6d6028fadb4b641a355
linux_china <libing.chen@gmail.com>
2022-01-21 13:58:52 -0800

    Polishing for host and path navigatiion

--- src/main/kotlin/org/jetbrains/plugins/rsocket/navigation/RSocketRoutingNavigation.kt
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/navigation/RSocketRoutingNavigation.kt
@@ -1,8 +1,10 @@
 package org.jetbrains.plugins.rsocket.navigation
 
+import com.intellij.httpClient.http.request.psi.HttpRequestTarget
 import com.intellij.navigation.DirectNavigationProvider
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiJavaFile
+import com.intellij.psi.util.parentOfType
 import org.jetbrains.plugins.rsocket.completion.RSocketRoutingCompletionContributor.Companion.rsocketRoutingCapture
 import org.jetbrains.plugins.rsocket.file.RSocketServiceFileIndex
 import org.jetbrains.plugins.rsocket.psi.extractAliRSocketService
@@ -13,41 +15,51 @@ import org.jetbrains.plugins.rsocket.psi.extractValueFromMessageMapping
 class RSocketRoutingNavigation : DirectNavigationProvider {
     override fun getNavigationElement(element: PsiElement): PsiElement? {
         if (rsocketRoutingCapture.accepts(element)) {
-            val rsocketRouting = element.text.trim('/')
-            if (rsocketRouting.isNotEmpty() && !rsocketRouting.contains("://")) {
-                RSocketServiceFileIndex.findRSocketServiceFiles(element.project).forEach { psiFile ->
-                    if (psiFile is PsiJavaFile) {
-                        val psiJavaClass = psiFile.classes[0]
-                        val rsocketService = psiJavaClass.hasAnnotation("com.alibaba.rsocket.RSocketService")
-                        if (rsocketService) {
-                            val aliRSocketService = extractAliRSocketService(psiJavaClass)
-                            val serviceFullName = aliRSocketService.serviceName
-                            if (rsocketRouting.startsWith(serviceFullName)) {
-                                aliRSocketService.serviceInterface
-                                    .methods
-                                    .forEach {
-                                        val routingKey = "${serviceFullName}.${it.name}"
-                                        if (routingKey == rsocketRouting) {
-                                            return it
+            val httpRequestTarget = element.parentOfType<HttpRequestTarget>()!!
+            val host = httpRequestTarget.host
+            val pathAbsolute = httpRequestTarget.pathAbsolute
+            if (host != null || pathAbsolute != null) {
+                val rsocketRouting = if (pathAbsolute != null) {
+                    val path = pathAbsolute.text
+                    path.substring(path.lastIndexOf('/') + 1)
+                } else {
+                    host!!.text
+                }
+                if (rsocketRouting.isNotEmpty()) {
+                    RSocketServiceFileIndex.findRSocketServiceFiles(element.project).forEach { psiFile ->
+                        if (psiFile is PsiJavaFile) {
+                            val psiJavaClass = psiFile.classes[0]
+                            val rsocketService = psiJavaClass.hasAnnotation("com.alibaba.rsocket.RSocketService")
+                            if (rsocketService) {
+                                val aliRSocketService = extractAliRSocketService(psiJavaClass)
+                                val serviceFullName = aliRSocketService.serviceName
+                                if (rsocketRouting.startsWith(serviceFullName)) {
+                                    aliRSocketService.serviceInterface
+                                        .methods
+                                        .forEach {
+                                            val routingKey = "${serviceFullName}.${it.name}"
+                                            if (routingKey == rsocketRouting) {
+                                                return it
+                                            }
                                         }
-                                    }
-                            }
-                        } else {
-                            var baseNameSpace = extractValueFromMessageMapping(psiJavaClass)
-                            baseNameSpace = if (baseNameSpace == null) {
-                                ""
+                                }
                             } else {
-                                "${baseNameSpace}."
-                            }
-                            if (baseNameSpace.isEmpty() || rsocketRouting.startsWith(baseNameSpace)) {
-                                psiJavaClass.methods
-                                    .forEach {
-                                        val mappingValue = extractValueFromMessageMapping(it) ?: it.name
-                                        val routingKey = "${baseNameSpace}${mappingValue}"
-                                        if (routingKey == rsocketRouting) {
-                                            return it
+                                var baseNameSpace = extractValueFromMessageMapping(psiJavaClass)
+                                baseNameSpace = if (baseNameSpace == null) {
+                                    ""
+                                } else {
+                                    "${baseNameSpace}."
+                                }
+                                if (baseNameSpace.isEmpty() || rsocketRouting.startsWith(baseNameSpace)) {
+                                    psiJavaClass.methods
+                                        .forEach {
+                                            val mappingValue = extractValueFromMessageMapping(it) ?: it.name
+                                            val routingKey = "${baseNameSpace}${mappingValue}"
+                                            if (routingKey == rsocketRouting) {
+                                                return it
+                                            }
                                         }
-                                    }
+                                }
                             }
                         }
                     }
