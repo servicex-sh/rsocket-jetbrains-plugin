2b57a01f3ee01565fd3ff85382e36ed7d729ea53
linux_china <libing.chen@gmail.com>
2022-02-06 19:03:40 -0800

    RSocketRoutingHttpIndex

---
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/file/RSocketRoutingHttpIndex.kt
@@ -0,0 +1,52 @@
+package org.jetbrains.plugins.rsocket.file
+
+import com.intellij.openapi.project.Project
+import com.intellij.util.indexing.*
+import com.intellij.util.io.EnumeratorStringDescriptor
+import com.intellij.util.io.KeyDescriptor
+import org.jetbrains.kotlin.utils.keysToMap
+import org.jetbrains.plugins.rsocket.RSOCKET_REQUEST_TYPES
+
+class RSocketRoutingHttpIndex : ScalarIndexExtension<String>() {
+    override fun getName() = NAME
+
+    override fun getIndexer(): DataIndexer<String, Void, FileContent> {
+        return DataIndexer { fileContent ->
+            fileContent.contentAsText.lines()
+                .filter { it.contains(' ') }
+                .filter { RSOCKET_REQUEST_TYPES.contains(it.substring(0, it.indexOf(' '))) }
+                .map {
+                    val routing = it.substring(it.indexOf(' ')).trim()
+                    if (routing.contains('/')) {
+                        routing.substring(routing.lastIndexOf('/') + 1)
+                    }
+                    routing
+                }.keysToMap { null }
+
+        }
+    }
+
+    override fun getKeyDescriptor(): KeyDescriptor<String> {
+        return EnumeratorStringDescriptor.INSTANCE
+    }
+
+    override fun getVersion() = 0
+
+    override fun getInputFilter(): FileBasedIndex.InputFilter {
+        return FileBasedIndex.InputFilter {
+            it.name.endsWith(".http") || it.name.endsWith(".rest")
+        }
+    }
+
+    override fun dependsOnFileContent() = true
+
+    companion object {
+        val NAME = ID.create<String, Void?>("rsocket.routingHttpIndex")
+
+        fun findAllRSocketRouting(project: Project): Collection<String> {
+            val fileBasedIndex = FileBasedIndex.getInstance()
+            return fileBasedIndex.getAllKeys(NAME, project)
+        }
+    }
+
+}
