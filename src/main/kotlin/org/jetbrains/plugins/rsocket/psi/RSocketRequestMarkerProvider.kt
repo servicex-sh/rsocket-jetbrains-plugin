8246a68386cee7afb91093d24313638320077789
linux_china <libing.chen@gmail.com>
2022-02-07 00:25:49 -0800

    Add kotlin support

--- src/main/kotlin/org/jetbrains/plugins/rsocket/psi/RSocketRequestMarkerProvider.kt
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/psi/RSocketRequestMarkerProvider.kt
@@ -1,52 +1,42 @@
 package org.jetbrains.plugins.rsocket.psi
 
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
-import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
-import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
-import com.intellij.httpClient.http.request.HttpRequestFileType
-import com.intellij.httpClient.http.request.psi.HttpRequestBlock
 import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiManager
-import org.jetbrains.kotlin.psi.psiUtil.getChildrenOfType
+import org.jetbrains.kotlin.asJava.toLightAnnotation
+import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
 import org.jetbrains.plugins.rsocket.file.RSocketRoutingHttpIndex
 import org.jetbrains.plugins.rsocket.messageMappingFullName
 
 
-class RSocketRequestMarkerProvider : RelatedItemLineMarkerProvider() {
+class RSocketRequestMarkerProvider : RSocketRequestBaseMarkerProvider() {
 
     override fun collectNavigationMarkers(element: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<*>>) {
+        var psiAnnotation: PsiAnnotation? = null
         if (element is PsiAnnotation) {
-            val psiAnnotation: PsiAnnotation = element
-            if (messageMappingFullName == element.qualifiedName) {
+            psiAnnotation = element
+        } else if (element is KtAnnotationEntry) {
+            psiAnnotation = element.toLightAnnotation()
+        }
+        if (psiAnnotation != null) {
+            if (messageMappingFullName == psiAnnotation.qualifiedName) {
                 var routing = psiAnnotation.findAttributeValue("value")?.text?.trim('"')
-                val psiClass = element.getParentOfType<PsiClass>(true)!!
-                if (psiClass.hasAnnotation(messageMappingFullName)) {
+                val psiClass = psiAnnotation.getParentOfType<PsiClass>(true)
+                if (psiClass != null && psiClass.hasAnnotation(messageMappingFullName)) {
                     val namespace = extractValueFromMessageMapping(psiClass)
                     if (namespace != null) {
                         routing = "${namespace}.${routing}"
                     }
                 }
-                val project = element.project
-                if (RSocketRoutingHttpIndex.findAllRSocketRouting(project).contains(routing)) {
-                    val httpFiles = RSocketRoutingHttpIndex.findHttpFiles(project, routing!!)
-                    if (httpFiles.isNotEmpty()) {
-                        val targets = mutableListOf<PsiElement>()
-                        httpFiles.forEach { virtualFile ->
-                            val psiFile = PsiManager.getInstance(element.project).findFile(virtualFile)
-                            psiFile?.getChildrenOfType<HttpRequestBlock>()?.forEach {
-                                val requestTarget = it.request.requestTarget!!.text
-                                if (requestTarget.contains(routing)) {
-                                    targets.add(it.request)
-                                }
-                            }
+                if (routing != null) {
+                    val project = element.project
+                    if (RSocketRoutingHttpIndex.findAllRSocketRouting(project).contains(routing)) {
+                        val navigationBuilder = rsocketRequestsNavigationBuilder(project, routing)
+                        if (navigationBuilder != null) {
+                            result.add(navigationBuilder.createLineMarkerInfo(element))
                         }
-                        val builder: NavigationGutterIconBuilder<PsiElement> = NavigationGutterIconBuilder.create(HttpRequestFileType.INSTANCE.icon)
-                            .setTargets(targets)
-                            .setTooltipText("Navigate to RSocket request")
-                        result.add(builder.createLineMarkerInfo(element))
                     }
                 }
             }
