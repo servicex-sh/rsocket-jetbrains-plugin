b895a3cb1a6513e63b76db15a896521ea1ef1a56
linux_china <libing.chen@gmail.com>
2022-02-08 14:25:50 -0800

    Polishing

--- src/main/kotlin/org/jetbrains/plugins/rsocket/RSocket.kt
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/RSocket.kt
@@ -3,7 +3,7 @@ package org.jetbrains.plugins.rsocket
 import com.intellij.openapi.util.IconLoader
 
 val rsocketIcon = IconLoader.findIcon("rsocket-icon.svg")!!
-val messageMappingFullName = "org.springframework.messaging.handler.annotation.MessageMapping"
-val rsocketServiceFullName = "com.alibaba.rsocket.RSocketService"
+const val messageMappingFullName = "org.springframework.messaging.handler.annotation.MessageMapping"
+const val rsocketServiceFullName = "com.alibaba.rsocket.RSocketService"
 
-val RSOCKET_REQUEST_TYPES = listOf("RSOCKET", "RPC", "FNF", "STREAM", "METADATA")
+val RSOCKET_REQUEST_TYPES = setOf("RSOCKET", "RPC", "FNF", "STREAM", "METADATA")
