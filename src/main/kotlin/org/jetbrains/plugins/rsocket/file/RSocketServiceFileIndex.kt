a429a8bb5c3c7ac2aa3038c6bf03bc3b83ee908d
linux_china <libing.chen@gmail.com>
2022-02-08 16:31:27 -0800

    Add findRelatedElement

--- src/main/kotlin/org/jetbrains/plugins/rsocket/file/RSocketServiceFileIndex.kt
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/file/RSocketServiceFileIndex.kt
@@ -2,12 +2,17 @@ package org.jetbrains.plugins.rsocket.file
 
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.util.indexing.*
 import com.intellij.util.io.EnumeratorStringDescriptor
 import com.intellij.util.io.KeyDescriptor
+import org.jetbrains.plugins.rsocket.psi.extractAliRSocketService
+import org.jetbrains.plugins.rsocket.psi.extractFirstClassFromJavaOrKt
+import org.jetbrains.plugins.rsocket.psi.extractValueFromMessageMapping
+import org.jetbrains.plugins.rsocket.rsocketServiceFullName
 
 class RSocketServiceFileIndex : ScalarIndexExtension<String>() {
     override fun getName() = NAME
@@ -50,6 +55,47 @@ class RSocketServiceFileIndex : ScalarIndexExtension<String>() {
                     .map { psiManager.findFile(it)!! }
                     .toList()
             }
+        }
+
+        fun findRelatedElement(project: Project, rsocketRouting: String): PsiElement? {
+            findRSocketServiceFiles(project).forEach { psiFile ->
+                val psiJavaClass = extractFirstClassFromJavaOrKt(psiFile)
+                if (psiJavaClass != null) {
+                    val rsocketService = psiJavaClass.hasAnnotation(rsocketServiceFullName)
+                    if (rsocketService) {
+                        val aliRSocketService = extractAliRSocketService(psiJavaClass)
+                        val serviceFullName = aliRSocketService.serviceName
+                        if (rsocketRouting.startsWith(serviceFullName)) {
+                            psiJavaClass
+                                .methods
+                                .forEach {
+                                    val routingKey = "${serviceFullName}.${it.name}"
+                                    if (routingKey == rsocketRouting) {
+                                        return it
+                                    }
+                                }
+                        }
+                    } else {
+                        var baseNameSpace = extractValueFromMessageMapping(psiJavaClass)
+                        baseNameSpace = if (baseNameSpace == null) {
+                            ""
+                        } else {
+                            "${baseNameSpace}."
+                        }
+                        if (baseNameSpace.isEmpty() || rsocketRouting.startsWith(baseNameSpace)) {
+                            psiJavaClass.methods
+                                .forEach {
+                                    val mappingValue = extractValueFromMessageMapping(it) ?: it.name
+                                    val routingKey = "${baseNameSpace}${mappingValue}"
+                                    if (routingKey == rsocketRouting) {
+                                        return it
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+            return null
         }
     }
 }
