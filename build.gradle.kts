9add47d90a47d4f07b73e7fc2ab5d8c6b639d25f
linux_china <libing.chen@gmail.com>
2022-04-20 12:04:16 -0700

    Exclude slf4j-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
 }
 
 configurations.implementation {
+    exclude(group = "org.slf4j", module = "slf4j-api")
     exclude(group = "io.netty", module = "netty-resolver-dns-native-macos")
     exclude(group = "io.projectreactor.netty.incubator", module = "reactor-netty-incubator-quic")
     exclude(group = "io.projectreactor.netty", module = "reactor-netty-http-brave")
