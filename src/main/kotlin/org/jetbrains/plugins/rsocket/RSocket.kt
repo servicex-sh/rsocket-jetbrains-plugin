8d551f85b5e131dacb8f5f0cd525cbb2ed1a542f
linux_china <libing.chen@gmail.com>
2022-04-20 19:03:44 -0700

    GRAPHQLRS

--- src/main/kotlin/org/jetbrains/plugins/rsocket/RSocket.kt
+++ src/main/kotlin/org/jetbrains/plugins/rsocket/RSocket.kt
@@ -6,4 +6,4 @@ val rsocketIcon = IconLoader.getIcon("rsocket-icon.svg", RSocketBundle::class.ja
 const val messageMappingFullName = "org.springframework.messaging.handler.annotation.MessageMapping"
 const val rsocketServiceFullName = "com.alibaba.rsocket.RSocketService"
 
-val RSOCKET_REQUEST_TYPES = setOf("RSOCKET", "RPC", "FNF", "STREAM", "METADATA")
+val RSOCKET_REQUEST_TYPES = setOf("RSOCKET", "RPC", "FNF", "STREAM", "METADATA","GRAPHQLRS")
