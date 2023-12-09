c05f455db705807f8e6e602a3f28514e7bf72495
Libing Chen <libing.chen@gmail.com>
2023-12-09 13:28:49 +0800

    build: update to netty 4.1.99

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ intellij {
 }
 
 dependencies {
-    implementation(platform("io.netty:netty-bom:4.1.101.Final"))
+    implementation(platform("io.netty:netty-bom:4.1.99.Final"))
     implementation(platform("io.projectreactor:reactor-bom:2020.0.38"))
     implementation(platform("com.fasterxml.jackson:jackson-bom:2.15.2"))
     implementation("io.rsocket:rsocket-core:1.1.4")
